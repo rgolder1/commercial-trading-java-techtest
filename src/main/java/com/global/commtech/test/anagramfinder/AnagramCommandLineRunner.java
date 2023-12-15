fa030f942a5469072efab63c1efcd980eae61392
rgolder1 <rgolder1@yahoo.com>
2023-12-15 11:05:29 +0000

    Base

---
+++ src/main/java/com/global/commtech/test/anagramfinder/AnagramCommandLineRunner.java
@@ -0,0 +1,21 @@
+package com.global.commtech.test.anagramfinder;
+
+import java.io.File;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+
+@Component
+@RequiredArgsConstructor
+public class AnagramCommandLineRunner implements CommandLineRunner {
+
+    @Override
+    public void run(final String... args) throws Exception {
+        Assert.isTrue(args.length == 1, "Please ensure that the input file is provided");
+
+        final File file = new File(args[0]);
+        Assert.isTrue(file.exists(), args[0] + " Does not exist");
+
+    }
+}
